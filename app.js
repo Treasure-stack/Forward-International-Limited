7659958c1b2b74a2592f565371a3cad6d0eb0084
Treasure's Dell <peacemary259@gmail.com>
2024-05-29 19:21:43 +0100

    initialcommit

---
+++ app.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('.toggle-menu')
+const closeIcon = document.querySelector('.times')
+const dropDown = document.querySelector('.bars')
+
+
+dropDown.addEventListener('click',()=>{
+  menu.style.display = 'block'
+  dropDown.style.display = 'none'
+})
+
+closeIcon.addEventListener('click',()=>{
+  menu.style.display= 'none'
+  dropDown.style.display='block'
+})
